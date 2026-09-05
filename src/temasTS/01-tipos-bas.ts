0ff800c9290daad337e37b84cc4f4546e8c29d77
IEVN1001-23000946 <82784@alumnos.utleon.edu.mx>
2026-09-05 09:58:12 -0600

    Operaciones basicas 05-09-26

---
+++ src/temasTS/01-tipos-bas.ts
@@ -0,0 +1,17 @@
+console.log("Hola mundo")
+
+var num1=23 //Esta en des uso
+let num2=25 //Usar este sobre todo
+const num3=22
+
+//Es un lenguaje tipado, siempre se debe de establecer el tipo de dato, automaticamente se toma un valor de entero con los numeros.
+
+let nombres:string //PAra declarar tipo de variable se pone ":" 
+let num:number
+let activo:boolean
+
+//no es nesesario declarar la variable para poderla usar antes, pero es más versatil
+
+let suma:number = num1+num2+num3
+
+console.log('La suma es: ' + suma)
